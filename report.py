33d717301aa9b48403a7a03759e813c3d23f818e
Beau Kinstler <git@beaukinstler.info>
2017-12-04 16:31:47 -0500

    Refactor to create views if not there and add function to report all reports

--- report.py
+++ report.py
@@ -2,7 +2,7 @@ import psycopg2
 import inspect
 import reportdb
 
-DEBUG=1
+DEBUG=0
 
 def debugger():
     print("called {}".format(inspect.stack()[1][3]))
@@ -12,7 +12,7 @@ def test_function():
     if (DEBUG==1):
         debugger()
 
-def print_rows(table):
+def print_10_rows(table):
     """print first 10 rows from the table"""
     rows = reportdb.get_data(table)
     i = 0
@@ -20,19 +20,8 @@ def print_rows(table):
         print(rows[i][-2]) # testing TODO: remove this. It shows that I can access column 4 from a resulting table
         i+=1
 
-def print_report():
-    """print first 10 rows from the table"""
-    rows = reportdb.report_authors_article_count()
-    i = 0
-    while (i < 10 and i < len(rows)):
-        print(str(rows[i][-2]) + " - " + str(rows[i][-1])) # testing TODO: remove this. It shows that I can access column 4 from a resulting table
-        i+=1
-        
-
-
-
 def main():
-    reportdb.create_view_author_article_count()
+    reportdb.create_views()
     if (DEBUG==1):
         debugger()
         test_function()
@@ -40,7 +29,7 @@ def main():
     #print_rows("log")
     # print_rows("articles")
     # print_rows("authors")
-    print_report()
+    reportdb.print_report_summary(100)
     
 
 
--- reportdb.py
+++ reportdb.py
@@ -25,28 +25,72 @@ def add_row(table,content):
   db.close()
 
 
-def create_view_author_article_count():
-  """Build the view of author ariticle counts"""
-
-  """Make sure the view doesn't already exist"""
+def check_view_exists(view_name):
   db = psycopg2.connect("dbname={}".format(DBNAME))
   c = db.cursor()
   c.execute("select * from information_schema.views    \
-             where table_name = 'v_author_article';")
-  
-  """If the view is found, stop the fuction"""
+             where table_name = '{}';".format(view_name))
+
   rows = c.fetchall()
-  if len(rows) > 0:
-    db.close()
-    return
-  
-  """Build the view"""
-  c.execute("create view v_author_article as select authors.name, count(articles.author) as num_of_articles \
-                from authors \
-                left outer join articles on authors.id = articles.author \
-                group by authors.name order by num_of_articles desc;")
-  db.commit()
   db.close()
+
+  if len(rows) > 0:
+    return True
+  else:
+    return False
+
+def create_views():
+  """Build the views used for reports"""
+
+  """Make sure the v_author_article doesn't already exist"""
+  if check_view_exists("v_author_article") == True:
+    print("found v_author_article")
+  else:
+    db = psycopg2.connect("dbname={}".format(DBNAME))
+    c = db.cursor()
+    
+    """Build the view"""
+    c.execute("create view v_author_article as select authors.name, count(articles.author) as num_of_articles \
+                  from authors \
+                  left outer join articles on authors.id = articles.author \
+                  group by authors.name order by num_of_articles desc;")
+    db.commit()
+    db.close()
+
+  """Make sure the v_most_viewed_article doesn't already exist"""
+  if check_view_exists("v_most_viewed_article") == True:
+    print("found v_most_viewed_article")
+  else:
+    db = psycopg2.connect("dbname={}".format(DBNAME))
+    c = db.cursor()
+
+    """Build the view"""
+    c.execute("create view v_most_viewed_article as select a.title, \
+                count(l.time) as views from articles a \
+                left join log l on substr(l.path,10) = a.slug \
+                group by a.title order by views desc;")
+    db.commit()
+    db.close()
+
+  """Make sure the v_report_errors doesn't already exist"""
+  if check_view_exists("v_report_errors") == True:
+    print("found v_report_errors")
+  else:
+    db = psycopg2.connect("dbname={}".format(DBNAME))
+    c = db.cursor()
+
+    """Build the view"""
+    c.execute("create view v_report_errors as select *, \
+                to_char(date,'Mon DD, YYYY') as friendly_date \
+                from (select date, round((err+0.0)/ok,4)*100 as percent \
+                from (select date(log.time),count(ok.id) as ok \
+                , count(notok.id) as err  from log \
+                left join log ok on ok.status = '200 OK' and ok.id = log.id \
+                left join log notok on notok.status <> '200 OK' and \
+                notok.id = log.id group by date(log.time)) as tbl) \
+                as tbl2 where percent > 1;")
+    db.commit()
+    db.close()
 
 def report_authors_article_count():
   """get the Authors' article counts"""
@@ -56,3 +100,45 @@ def report_authors_article_count():
   rows = c.fetchall()
   db.close()
   return(rows)
+
+def report_articles_most_viewied():
+  """get the Authors' article counts"""
+  db = psycopg2.connect("dbname={}".format(DBNAME))
+  c = db.cursor()
+  c.execute("select * from v_most_viewed_article;")
+  rows = c.fetchall()
+  db.close()
+  return(rows)
+
+def report_http_errors():
+  """report on dates with more than 1% of requests returning errors"""
+  db = psycopg2.connect("dbname={}".format(DBNAME))
+  c = db.cursor()
+  c.execute("select friendly_date, cast(round(percent,2) as text)||'%' as error from v_report_errors;")
+  rows = c.fetchall()
+  db.close()
+  return(rows)
+
+def print_report_summary(rows_to_print):
+    """print first rows_to_print rows from the table"""
+    rows = report_authors_article_count()
+    i = 0
+    while (i < rows_to_print and i < len(rows)):
+        print(str(rows[i][-2]) + " - " + str(rows[i][-1])) # testing TODO: remove this. It shows that I can access column 4 from a resulting table
+        i+=1
+
+    """print first rows_to_print rows from the table"""
+    rows = report_articles_most_viewied()
+    i = 0
+    
+    while (i < rows_to_print and i < len(rows)):
+        print('"' + str(rows[i][-2]) + '"' + " - " + str(rows[i][-1])) # testing TODO: remove this. It shows that I can access column 4 from a resulting table
+        i+=1
+
+    rows = report_http_errors()
+    i = 0
+    
+    while (i < len(rows)):
+        print(str(rows[i][-2]) + " - " + str(rows[i][-1])) # testing TODO: remove this. It shows that I can access column 4 from a resulting table
+        i+=1
+

aa1a7e4472c38fbb7c79d3025967521f1e5aa6f0
Beau Kinstler <git@beaukinstler.info>
2017-12-10 09:11:15 -0500

    Add better error checking a messges

--- report.py
+++ report.py
@@ -26,20 +26,31 @@ def main():
     """main program"""
 
     if ARGS.createviews:
-        reportdb.create_views()
+        print("Attempting to create and/or test the views. \
+              You should see a list of author names if successful.")
 
         # Test a call to the db
         try:
+            reportdb.create_views()
             print_10_rows("v_author_article")
             print("Views created and tested")
         except Exception as e:
             print("There was a problem connecting \
                   to the 'v_author_article' view. \
                   there may be a problem with \
-                  the database or views didn't build correctly.")
+                  the database or views didn't build correctly. \
+                  An error message should follow with technical details")
             print(e.args[0])
         return
-
+    else:
+        try:
+            if reportdb.check_view_exists("v_author_article") is False:
+                print("Something went wrong. Perhaps try to build the views by running with the --createviews flag")
+                return 1   
+        except:
+            print("Something went wrong. Make sure the reportdb.py exists in the same directory as report.py")
+                
+            
     reportdb.print_report_summary(100)
 
 if __name__ == '__main__':
